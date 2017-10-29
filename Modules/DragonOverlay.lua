403685acfb3fc6bef0d969eac3e67fbf4c9a36d4
Azilroka <darktemplar9900@yahoo.com>
2017-10-29 16:27:40 -0500

    Dragon Overlay Fixes

--- Modules/DragonOverlay.lua
+++ Modules/DragonOverlay.lua
@@ -3,6 +3,11 @@ local PA = _G.ProjectAzilroka
 local DO = LibStub('AceAddon-3.0'):NewAddon('DragonOverlay', 'AceEvent-3.0')
 _G.DragonOverlay = DO
 
+local _G = _G
+local pairs, tinsert, select, unpack = pairs, tinsert, select, unpack
+local strfind, strsub = strfind, strsub
+local UnitIsPlayer, UnitClass, UnitClassification = UnitIsPlayer, UnitClass, UnitClassification
+
 DO.Title = '|cffC495DDDragonOverlay|r'
 DO.Authors = 'Azilroka    Infinitron'
 DO.ImageCredits = 'Codeblake    Kkthnxbye    Narley    Durandil'
@@ -112,7 +117,7 @@ function DO:GetOptions()
 			name = Name,
 			guiInline = true,
 			get = function(info) return DO.db[Option][info[#info]] end,
-			set = function(info, value) DO.db[Option][info[#info]] = value DO:Update() end,
+			set = function(info, value) DO.db[Option][info[#info]] = value DO:SetOverlay() end,
 			args = {
 				point = {
 					name = 'Anchor Point',
@@ -222,25 +227,19 @@ function DO:GetOptions()
 		Order = Order + 2
 	end
 
-	if PA.EP then
-		PA.AceOptionsPanel.Options.args.DragonOverlay = Options
-	end
+	PA.AceOptionsPanel.Options.args.DragonOverlay = Options
 end
 
-function DO:Update()
-	self.frame:SetParent(_G[self.db['DragonPoints']['relativeTo']])
-	self:PLAYER_TARGET_CHANGED()
-end
-
-function DO:SetOverlay(Class)
+function DO:SetOverlay()
 	local Points = 'DragonPoints'
-	local Texture = self.db[TargetClass]
+	local TargetClass = UnitClassification('target')
+	local Texture = DO.Textures[self.db[TargetClass]]
 
 	if UnitIsPlayer('target') and self.db['ClassIcon'] then
-		local Class = select(2, UnitClass('target'))
+		TargetClass = select(2, UnitClass('target'))
 		self.frame:SetSize(32, 32)
 		self.frame.Texture:SetTexture([[Interface\WorldStateFrame\Icons-Classes]])
-		self.frame.Texture:SetTexCoord(unpack(CLASS_ICON_TCOORDS[Class]))
+		self.frame.Texture:SetTexCoord(unpack(CLASS_ICON_TCOORDS[TargetClass]))
 		Points = 'ClassIconPoints'
 	else
 		if Texture and strfind(Texture, 'Classic') then
@@ -248,7 +247,7 @@ function DO:SetOverlay(Class)
 		else
 			self.frame:SetSize(128, 32)
 		end
-		self.frame.Texture:SetTexture(Texture or nil)
+		self.frame.Texture:SetTexture(Texture)
 		self.frame.Texture:SetTexCoord(self.db['FlipDragon'] and 1 or 0, self.db['FlipDragon'] and 0 or 1, 0, 1)
 	end
 
@@ -259,63 +258,61 @@ function DO:SetOverlay(Class)
 	self.frame:SetFrameLevel(self.db['Level'])
 end
 
-function DO:PLAYER_TARGET_CHANGED()
-	self:SetOverlay(UnitClassification('target'))
-end
-
+local Defaults
 function DO:SetupProfile()
-	self.data = LibStub('AceDB-3.0'):New('DragonOverlayDB', {
-		profile = {
-			['Strata'] = '2-MEDIUM',
-			['Level'] = 12,
-			['worldboss'] = 'Chromatic',
-			['elite'] = 'HeavenlyGolden',
-			['rare'] = 'Onyx',
-			['rareelite'] = 'HeavenlyOnyx',
-			['ClassIcon'] = false,
-			['ClassIconPoints'] = {
-				['point'] = 'CENTER',
-				['relativeTo'] = 'oUF_Target',
-				['relativePoint'] = 'TOP',
-				['xOffset'] = 0,
-				['yOffset'] = 5,
+	if not Defaults then
+		Defaults = {
+			profile = {
+				['Strata'] = '2-MEDIUM',
+				['Level'] = 12,
+				['worldboss'] = 'Chromatic',
+				['elite'] = 'HeavenlyGolden',
+				['rare'] = 'Onyx',
+				['rareelite'] = 'HeavenlyOnyx',
+				['ClassIcon'] = false,
+				['ClassIconPoints'] = {
+					['point'] = 'CENTER',
+					['relativeTo'] = 'oUF_Target',
+					['relativePoint'] = 'TOP',
+					['xOffset'] = 0,
+					['yOffset'] = 5,
+				},
+				['DragonPoints'] = {
+					['point'] = 'CENTER',
+					['relativeTo'] = 'oUF_Target',
+					['relativePoint'] = 'TOP',
+					['xOffset'] = 0,
+					['yOffset'] = 5,
+				},
+				['FlipDragon'] = false,
 			},
-			['DragonPoints'] = {
-				['point'] = 'CENTER',
-				['relativeTo'] = 'oUF_Target',
-				['relativePoint'] = 'TOP',
-				['xOffset'] = 0,
-				['yOffset'] = 5,
-			},
-			['FlipDragon'] = false,
-		},
-	})
-
-	for _, Option in pairs({'ClassIconPoints', 'DragonPoints' }) do
-		if PA.Tukui then
-			self.data.profile[Option].relativeTo = 'oUF_TukuiTarget'
-		end
-		if PA.ElvUI then
-			self.data.profile[Option].relativeTo = 'ElvUF_Target'
-		end
-		if PA.NUI then
-			self.data.profile[Option].relativeTo = 'NenaUF_TargetVerticalUnitFrame'
+		}
+		for _, Option in pairs({'ClassIconPoints', 'DragonPoints' }) do
+			if PA.Tukui then
+				Defaults.profile[Option].relativeTo = 'oUF_TukuiTarget'
+			end
+			if PA.ElvUI then
+				Defaults.profile[Option].relativeTo = 'ElvUF_Target'
+			end
+			if PA.NUI then
+				Defaults.profile[Option].relativeTo = 'NenaUF_TargetVerticalUnitFrame'
+			end
 		end
 	end
+
+	self.data = LibStub('AceDB-3.0'):New('DragonOverlayDB', Defaults)
+
 	self.data.RegisterCallback(self, 'OnProfileChanged', 'SetupProfile')
 	self.data.RegisterCallback(self, 'OnProfileCopied', 'SetupProfile')
 	self.db = self.data.profile
 end
 
 function DO:Initialize()
-	self:SetupProfile()
-
 	local frame = CreateFrame("Frame", 'DragonOverlayFrame', UIParent)
 	frame.Texture = frame:CreateTexture(nil, 'ARTWORK')
 	frame.Texture:SetAllPoints()
-	--frame:SetParent(self.db['DragonPoints']['relativeTo'] or UIParent)
-	frame:SetFrameLevel(12)
 	self.frame = frame
 
-	self:RegisterEvent('PLAYER_TARGET_CHANGED')
+	self:RegisterEvent('PLAYER_ENTERING_WORLD', 'SetupProfile')
+	self:RegisterEvent('PLAYER_TARGET_CHANGED', 'SetOverlay')
 end
