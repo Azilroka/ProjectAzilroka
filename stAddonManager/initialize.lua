37446c74e9119af49bb82e9b367b2d71de1478cf
Azilroka <darktemplar9900@yahoo.com>
2017-12-07 00:38:33 -0600

    1.11
    To fix LibElvUIPlugin

--- stAddonManager/initialize.lua
+++
@@ -1,9 +0,0 @@
-local _, st = ...
-local stAM = CreateFrame("Frame", "stAddonManager", UIParent)
-st[1] = stAM -- for local usage
-
-st.FontStringTable = {} --Store all fontstrings in here (used by stAPI)
-
-stAM.buttonHeight = 18
-stAM.buttonWidth = 22
-stAM.scrollOffset = 0
