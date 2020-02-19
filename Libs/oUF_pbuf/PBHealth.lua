7f1c36e56fc31da2ad4898aec056236d281d3968
NihilisticPandemonium <whiro@tukui.org>
2020-02-19 01:16:12 -0800

    These colors don't make sense here.

--- Libs/oUF_pbuf/PBHealth.lua
+++ Libs/oUF_pbuf/PBHealth.lua
@@ -62,10 +62,6 @@ The following options are listed by priority. The first check that returns true 
     Background:SetTexture(1, 1, 1, .5)
 
     -- Options
-    Health.colorTapping = true
-    Health.colorDisconnected = true
-    Health.colorClass = true
-    Health.colorReaction = true
     Health.colorHealth = true
 
     -- Make the background darker.
