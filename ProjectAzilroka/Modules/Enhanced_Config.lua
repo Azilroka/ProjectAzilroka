ef5815e51151c71462913f6aebbf0c4989c2f35a
Azilroka <darktemplar9900@yahoo.com>
2021-07-12 16:51:51 -0500

    Enhanced Config

--- ProjectAzilroka/Modules/Enhanced_Config.lua
+++ ProjectAzilroka/Modules/Enhanced_Config.lua
@@ -15,25 +15,9 @@ for _, devName in pairs(DEVELOPERS) do
 	DEVELOPER_STRING = DEVELOPER_STRING..'|n'..devName
 end
 
-EC.Options = PA.ACH:Group(EC.Title, nil, 4)
+EC.Options = PA.ACH:Group('', nil, 4)
 EC.Options.args.credits = PA.ACH:Group('Credits', nil, -1)
 EC.Options.args.credits.args.text = PA.ACH:Description('Coding:\n'..DEVELOPER_STRING, 1, 'medium')
-
-function EC:PositionGameMenuButton()
-	_G.GameMenuFrame:SetHeight(_G.GameMenuFrame:GetHeight() + _G.GameMenuButtonLogout:GetHeight() - 4)
-
-	if PA.Tukui and _G.Tukui[1].Miscellaneous.GameMenu.Tukui then
-		_G.GameMenuFrame:SetHeight(_G.GameMenuFrame:GetHeight() + _G.GameMenuButtonLogout:GetHeight() - 4)
-	end
-
-	local _, relTo, _, _, offY = _G.GameMenuButtonLogout:GetPoint()
-	if relTo ~= _G.GameMenuFrame['EC'] then
-		_G.GameMenuFrame['EC']:ClearAllPoints()
-		_G.GameMenuFrame['EC']:SetPoint("TOPLEFT", PA.Tukui and _G.Tukui[1].Miscellaneous.GameMenu.Tukui or relTo, "BOTTOMLEFT", 0, -1)
-		_G.GameMenuButtonLogout:ClearAllPoints()
-		_G.GameMenuButtonLogout:SetPoint("TOPLEFT", _G.GameMenuFrame['EC'], "BOTTOMLEFT", 0, offY)
-	end
-end
 
 function EC.OnConfigClosed(widget)
 	PA.ACD.OpenFrames['Enhanced_Config'] = nil
@@ -55,28 +39,11 @@ function EC:ToggleConfig()
 end
 
 function EC:Initialize()
-	local GameMenuButton = _G.CreateFrame("Button", nil, _G.GameMenuFrame, "GameMenuButtonTemplate")
-	GameMenuButton:SetText(EC.Title)
-	GameMenuButton:SetScript("OnClick", function()
-		EC:ToggleConfig()
-		_G.HideUIPanel(_G.GameMenuFrame)
-	end)
-	_G.GameMenuFrame['EC'] = GameMenuButton
-
-	if PA.Tukui then
-		_G.Tukui[1].Miscellaneous.GameMenu.EnableTukuiConfig = function() end
-		_G.Tukui[1].Miscellaneous.GameMenu.AddHooks = function() end
-	end
-
-	if not _G.IsAddOnLoaded("ConsolePortUI_Menu") then
-		GameMenuButton:SetSize(_G.GameMenuButtonLogout:GetWidth(), _G.GameMenuButtonLogout:GetHeight())
-		GameMenuButton:SetPoint("TOPLEFT", _G.GameMenuButtonAddons, "BOTTOMLEFT", 0, -1)
-		_G.hooksecurefunc('GameMenuFrame_UpdateVisibleButtons', EC.PositionGameMenuButton)
-	end
-
 	PA.AC:RegisterOptionsTable('Enhanced_Config', EC.Options)
 	PA.ACD:SetDefaultSize('Enhanced_Config', 1200, 800)
-	EC:RegisterChatCommand('ec', 'ToggleConfig')
+	EC:RegisterChatCommand('pa', 'ToggleConfig')
+
+	PA.ACD:AddToBlizOptions('Enhanced_Config', 'ProjectAzilroka')
 
 	EC:RegisterChatCommand('rl', ReloadUI)
 	EC:RegisterChatCommand('reloadui', ReloadUI)
