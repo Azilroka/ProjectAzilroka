bfd9738275a93f63d817094a2e99c45cd238a750
Azilroka <darktemplar9900@yahoo.com>
2023-01-28 18:14:20 -0600

    just check it here

--- ProjectAzilroka/Modules/SquareMinimapButtons.lua
+++ ProjectAzilroka/Modules/SquareMinimapButtons.lua
@@ -291,11 +291,6 @@ function SMB:HandleBlizzardButtons()
 end
 
 function SMB:SkinMinimapButton(button)
-	if (not button) or button.isSkinned then return end
-
-	local name = button.GetName and button:GetName()
-	if not name then return end
-
 	for _, frames in next, { button, button:GetChildren() } do
 		for _, region in next, { frames:GetRegions() } do
 			if region.IsObjectType and region:IsObjectType('Texture') then
@@ -361,7 +356,7 @@ function SMB:GrabMinimapButtons(forceUpdate)
 	for _, btn in ipairs({Minimap:GetChildren()}) do
 		local name = btn.GetName and btn:GetName() or btn.name
 
-		if not (SMB.IgnoreButton[name] or btn.uiMapID or btn.waypoint or (btn.data and btn.data.UiMapID) or (not name and btn.icon) or (name and strmatch(name, "^QuestieFrame"))) then
+		if not (SMB.IgnoreButton[name] or btn.isSkinned or btn.uiMapID or btn.waypoint or (btn.data and btn.data.UiMapID) or (btn and not name) or (not name and btn.icon) or (name and strmatch(name, "^QuestieFrame"))) then
 			SMB:SkinMinimapButton(btn)
 			UpdateBar = true
 		end
