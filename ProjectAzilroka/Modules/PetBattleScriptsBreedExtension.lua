0792aac638b7f518bb91c42ddbb528df5a49e495
Nihilistzsche <nihilistzsche@gmail.com>
2025-06-26 03:15:46 -0700

    New Module: PetBattleScriptsBreedExtension
    When using PetBattleScripts and BattlePetBreedID it adds two conditions
    usable with PetBattleScripts [breed] and [breednum]
    
    Usable like [enemy.breed == "S/S"] or [enemy.breednum == 5]

---
+++ ProjectAzilroka/Modules/PetBattleScriptsBreedExtension.lua
@@ -0,0 +1,39 @@
+local PA, ACL, ACH = unpack(_G.ProjectAzilroka)
+if PA.Classic or not PA:IsAddOnEnabled("tdBattlePetScript") or not PA:IsAddOnEnabled("BattlePetBreedID") then
+	error("not loading PBSBE")
+	return
+end
+
+local PBSBE = PA:NewModule("PetBattleScriptsBreedExtension")
+local C_AddOns_IsAddOnLoaded = _G.C_AddOns.IsAddOnLoaded
+
+function PBSBE.GetBreedInfo(petOwner, petIndex, breedFormat)
+	local oldOpt = _G.BPBID_Options.format
+	_G.BPBID_Options.format = breedFormat
+	local breed = _G.GetBreedID_Battle({ petOwner = petOwner, petIndex = petIndex })
+	_G.BPDID_Options.formaat = oldOpt
+	return breed
+end
+
+function PBSBE.BreedText(owner, pet)
+	return PBSBE.GetBreedInfo(owner, pet, 3)
+end
+
+function PBSBE.Breed(owner, pet)
+	return PBSBE.GetBreedInfo(owner, pet, 1)
+end
+
+PBSBE.Conditions = {
+	breed = PBSBE.BreedText,
+	breednum = PBSBE.Breed,
+}
+
+function PBSBE:Initialize()
+	pcall(C_AddOns_LoadAddOn, "tdBattlePetScript")
+	if C_AddOns_IsAddOnLoaded("tdBattlePetScript") then
+		local PBS = _G.PetBattleScripts
+		for condition, func in pairs(self.Conditions) do
+			PBS:RegisterCondition(condition, { type = "compare", arg = false }, func)
+		end
+	end
+end
